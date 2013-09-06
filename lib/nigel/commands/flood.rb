a2315355498ebf364db9921e9a89331a5d05a99a
Zach Latta <zchlatta@gmail.com>
2013-09-05 19:49:39 -0700

    Optimize fetching process.

--- lib/nigel/commands/flood.rb
+++ lib/nigel/commands/flood.rb
@@ -3,20 +3,29 @@ module Nigel
     class Flood
       def self.process(options)
 
-        File.open('images.html', 'w+') do |file|
+        Google::Search::Image.new(
+          query: 'Nigel Thornberry').each_with_index do |image, i|
 
-          Google::Search::Image.new(
-            query: 'Nigel Thornberry').each_with_index do |image, i|
+          threads = []
 
-            if(image.uri.include? '.gif')
+          if(image.uri.include? '.gif')
 
-              puts "Nigel spotted at #{ image.uri }!"
-              File.open("#{ i }.gif", "wb") do |saved_file|
+            puts "Nigel spotted at #{ image.uri }!"
+            
+            threads << self.fetch(image.uri, "#{ i }.gif")
+          end
 
-                open(image.uri, 'rb') do |read_file|
-                  saved_file.write(read_file.read)
-                end
-              end
+          threads.each(&:join)
+        end
+      end
+
+      private
+
+      def self.fetch(uri, name)
+        Thread.new do
+          File.open(name, "wb") do |saved_file|
+            open(uri, 'rb') do |read_file|
+              saved_file.write(read_file.read)
             end
           end
         end
