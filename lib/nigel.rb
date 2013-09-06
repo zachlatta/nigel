5adef0cc6a43f98374fe22ef1ce29a0451616b76
Zach Latta <zchlatta@gmail.com>
2013-09-05 19:38:55 -0700

    Nigeling it up in here.

--- lib/nigel.rb
+++ lib/nigel.rb
@@ -1,5 +1,13 @@
-require "nigel/version"
 
-module Nigel
-  # Your code goes here...
+def require_all(path)
+  glob = File.join(File.dirname(__FILE__), path, '*.rb')
+  Dir[glob].each do |f|
+    require f
+  end
 end
+
+require 'nigel/version'
+require_all 'nigel/commands'
+
+require 'google-search'
+require 'open-uri'
--- lib/nigel/commands/flood.rb
+++ lib/nigel/commands/flood.rb
@@ -2,6 +2,24 @@ module Nigel
   module Commands
     class Flood
       def self.process(options)
+
+        File.open('images.html', 'w+') do |file|
+
+          Google::Search::Image.new(
+            query: 'Nigel Thornberry').each_with_index do |image, i|
+
+            if(image.uri.include? '.gif')
+
+              puts "Nigel spotted at #{ image.uri }!"
+              File.open("#{ i }.gif", "wb") do |saved_file|
+
+                open(image.uri, 'rb') do |read_file|
+                  saved_file.write(read_file.read)
+                end
+              end
+            end
+          end
+        end
       end
     end
   end
